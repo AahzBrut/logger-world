62827fb1c37458fc0b9d8412acd2c76f137d14a9
AahzBrut <partyomenko@gmail.com>
2020-11-16 19:15:24 +0300

    Add postman scripts to test REST endpoints

--- src/main/kotlin/io/github/loggerworld/repository/user_account/UserAccountRepository.kt
+++ src/main/kotlin/io/github/loggerworld/repository/user_account/UserAccountRepository.kt
@@ -2,7 +2,6 @@ package io.github.loggerworld.repository.user_account
 
 import io.github.loggerworld.domain.user_account.UserAccount
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
 interface UserAccountRepository : JpaRepository<UserAccount, Long> {
 
