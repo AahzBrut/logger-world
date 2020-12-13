441bfbf499c1708eacd51638c74627d6ef6e7b87
AahzBrut <partyomenko@gmail.com>
2020-12-13 18:45:27 +0300

    Allow access from all origins

--- src/main/kotlin/io/github/loggerworld/config/WebSecurityConfig.kt
+++ src/main/kotlin/io/github/loggerworld/config/WebSecurityConfig.kt
@@ -4,12 +4,15 @@ import io.github.loggerworld.controller.SIGN_UP_URL
 import io.github.loggerworld.controller.TEST_URL
 import io.github.loggerworld.service.security.JwtAuthenticationFilter
 import io.github.loggerworld.service.security.JwtAuthorizationFilter
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 
 @Configuration
@@ -22,7 +25,9 @@ class WebSecurityConfig(
     override fun configure(http: HttpSecurity) {
         http
             .headers().frameOptions().disable()
-            .and().cors().and().csrf().disable()
+            .and().cors()
+            .configurationSource(corsConfigurationSource())
+            .and().csrf().disable()
             .authorizeRequests()
             .antMatchers(SIGN_UP_URL).permitAll()
             .antMatchers(TEST_URL).authenticated()
@@ -33,5 +38,20 @@ class WebSecurityConfig(
             .addFilter(jwtAuthenticationFilter)
             .addFilter(jwtAuthorizationFilter)
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("*")
+        configuration.allowedMethods = listOf("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+        configuration.allowCredentials = true
+        //the below three lines will add the relevant CORS response headers
+        configuration.addAllowedOrigin("*")
+        configuration.addAllowedHeader("*")
+        configuration.addAllowedMethod("*")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
