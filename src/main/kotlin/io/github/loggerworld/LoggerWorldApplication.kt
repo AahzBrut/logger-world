ce240460a354bbec1a6d3e27ef4a6eb27088e6ce
AahzBrut <partyomenko@gmail.com>
2020-12-19 17:55:03 +0300

    Add tables to store items settings

--- src/main/kotlin/io/github/loggerworld/LoggerWorldApplication.kt
+++ src/main/kotlin/io/github/loggerworld/LoggerWorldApplication.kt
@@ -2,8 +2,10 @@ package io.github.loggerworld
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 import javax.annotation.PostConstruct
 
+@EnableScheduling
 @SpringBootApplication
 class LoggerWorldApplication
 
